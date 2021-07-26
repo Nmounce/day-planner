f4352659849d64ea5bcdce50f4742f0b13a8f5af
NikkiMounce <nikkimounce@gmail.com>
2021-07-25 21:26:13 -0700

    added styling and schedule grid

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //THEN the current day is displayed at the top of the calendar
 const currentDate = moment().format('MM/DD/YYYY'); // get current date
 document.getElementById("currentDay").textContent = currentDate; // display inside the element
+
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 
@@ -18,3 +19,7 @@ document.getElementById("currentDay").textContent = currentDate; // display insi
 // WHEN I refresh the page
 // THEN the saved events persist
 // ```
+
+//sources: 
+// http://5.9.10.113/68338961/moment-js-confusion-for-displaying-the-date
+// https://medium.com/@aleks.roslyakov/using-css-grid-jquery-making-a-daily-scheduler-pt-i-6ba0d2b37e6f
