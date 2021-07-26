27264cf2f37c85a48e27003f9523d6610992c22f
NikkiMounce <nikkimounce@gmail.com>
2021-07-25 20:21:09 -0700

    modified html to include grid, moment.js working

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+const currentDate = moment().format('MM/DD/YYYY'); // get current date
+document.getElementById("currentDay").textContent = currentDate; // display inside the element
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+// WHEN I refresh the page
+// THEN the saved events persist
+// ```
